fcf31d367645d6c09f2c293b10fdd78c8720d6c0
lucas leitao <lucas.leitao25@gmail.com>
2021-01-26 11:21:20 -0300

    receber imagens API

---
+++ ProjetoFinal/ProjetoFinal/Model/ImageModel.swift
@@ -0,0 +1,8 @@
+//
+//  ImageModel.swift
+//  ProjetoFinal
+//
+//  Created by Lucas Abdel Leitao on 22/01/21.
+//
+
+import Foundation
---
+++ ProjetoFinal/ProjetoFinal/Model/UIImageExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIImageExtension.swift
+//  ProjetoFinal
+//
+//  Created by Lucas Abdel Leitao on 26/01/21.
+//
+
+import Foundation
